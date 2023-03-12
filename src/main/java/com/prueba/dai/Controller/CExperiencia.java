04e5f7450fdcb9d9068916e08532aec867090992
Daiana <daianaerazo@gmail.com>
2023-03-12 13:16:53 -0500

    Re escritura de docker

--- src/main/java/com/prueba/dai/Controller/CExperiencia.java
+++ src/main/java/com/prueba/dai/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/experlabo")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = "''")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/prueba/dai/Security/Controller/AuthController.java
+++ src/main/java/com/prueba/dai/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200" , "https://porfolio-front.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200" , "https://backprueba-tnk1.onrender.com"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
